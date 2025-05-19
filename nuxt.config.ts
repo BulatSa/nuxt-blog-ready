c38ab3e64fa75b1231cc64ec6846050b7025f3ec
Bulat <sadikovba@gmail.com>
2025-05-19 17:40:54 +0300

    add ya metrika

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,14 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@formkit/auto-animate',
     '@stefanobartoletti/nuxt-social-share',
+    ['yandex-metrika-module-nuxt3',
+      {
+        id: '47293344',
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true,
+      }
+    ]
   ],
 
   app: {
